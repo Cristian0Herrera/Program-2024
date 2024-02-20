3c4a293c46c64fb491af4d4d7a48222b3501c404
ronalgranillo <ronaldgranillo49@gmail.com>
2024-02-19 22:07:10 -0600

    Creacion de los formulario de main y inscripcion

---
+++ main.js
@@ -0,0 +1,73 @@
+var db;
+
+        app = new Vue({
+            el:"#app",
+            data:{
+                forms:{
+                  alumno:{ mostrar:false, },
+                  materia:{ mostrar:false, },
+                  matricula:{ mostrar:false, },
+                  inscripcion:{ mostrar:false, },
+                  docente:{mostrar:false,}
+                }
+            },
+            methods: {
+              abrirFormulario(form){
+                // Ocultar todos los formularios
+                for (let key in this.forms) {
+                    if (key !== form) {
+                        this.forms[key].mostrar = false;
+                    }
+                }
+                // Mostrar el formulario seleccionado
+                this.forms[form].mostrar = true;
+                this.$refs[form].listar();
+              },
+              abrirBD(){
+                let indexDB = indexedDB.open('db_sistema_academico',1);
+                indexDB.onupgradeneeded = e=>{
+                  let req = e.target.result,
+                    tblalumnos = req.createObjectStore('tblalumnos',{keyPath: 'idAlumno'}),
+                    tblmaterias = req.createObjectStore('tblmaterias',{keyPath: 'idMateria'});
+                    tbldocentes = req.createObjectStore('tbldocentes',{keyPath:'idDocente'});
+                    tblmatriculas = req.createObjectStore('tblmatriculas',{keyPath:'idMatricula'});
+                    tblinscripcion = req.createObjectStore('tblinscripcion',{keyPath:'idInscripcion'});
+                    
+                    tblalumnos.createIndex('idAlumno','idAlumno',{unique:true});
+                    tblalumnos.createIndex('codigo','codigo',{unique:true});
+                    tbldocentes.createIndex('idDocente','idDocente',{unique:true});
+                    tblmaterias.createIndex('idMateria','idMateria',{unique:true});
+                    tblinscripcion.createIndex('idInscripcion','idInscripcion',{unique:true});
+                };
+                indexDB.onsuccess = e=>{
+                  db = e.target.result;
+                };
+                indexDB.onerror = e=>{
+                  console.error('ERROR al crear, abrir la BD',e);
+                };
+              }
+            },
+            created(){
+              this.abrirBD();
+            }
+        });
+
+        function abrirStore(store,modo){
+          let ltx = db.transaction(store,modo);
+          return ltx.objectStore(store);
+        }
+
+        async function seleccionarImagen(image){
+          let archivo = image.files[0];
+          if(archivo){
+              let blob = await img(archivo, 1),
+                  reader = new FileReader();
+              reader.onload = e=>{
+                app.$refs.matricula.matricula.comprobante = e.target.result;
+                console.log(e.target.result);
+              };
+              reader.readAsDataURL(blob);
+          }else{
+            console.log("Por favor seleccione una imagen válida...");
+          }
+        }
